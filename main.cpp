2b9d02c8157c67c09dcde6802aa188a8ff27e25a
whyKVD <faccincani.al@gmail.com>
2023-11-02 10:21:49 +0100

    finished the cli

--- main.cpp
+++ main.cpp
@@ -28,21 +28,98 @@ struct Trx* newTrx(double amount,
 }
 
 void stampTrx(Trx* trx){
-  cout << "Trx:{" << endl;
-  cout << "\tamount: " << trx->amount << "," << endl;
-  cout << "\tdateTime: " << trx->dateTime << "," << endl;
-  cout << "\tdescription: " << trx->description << "," << endl;
-  cout << "\tisExpense: " << trx->isExpense << "," << endl;
-  cout << "}" << endl;
+  cout << "\tTrx:{" << endl;
+  cout << "\t\tamount: " << trx->amount << "," << endl;
+  cout << "\t\tdateTime: " << trx->dateTime << "," << endl;
+  cout << "\t\tdescription: " << trx->description << "," << endl;
+  cout << "\t\tisExpense: " << trx->isExpense << "," << endl;
+  cout << "\t}";
 }
 
-void insertTrx(Trx *trx);
+struct Era{
+	Trx* currentTrx;
+ Era* nextEra;
+};
+
+struct Era* newEra(Trx* trx){
+  Era* era = new Era;
+	 era->currentTrx = trx;
+  era->nextEra = NULL;
+ 
+  return era;
+}
+
+struct TrxEras{
+	 Era *rootEra;
+};
+
+struct TrxEras* newTrxEras(Era *rootEra){
+  TrxEras* eras = new TrxEras;
+  eras->rootEra = rootEra;
+  
+  return eras;
+}
+
+void totalExp(TrxEras* eras){
+  double exp = .0, earn = .0;
+  Era* root = eras->rootEra;
+  if(root == NULL){
+    cout << "none expense or earning inserted" << endl;
+    return;
+  }
+  
+  while(root->nextEra != NULL){
+    Trx* trx = root->currentTrx;
+    if(trx->isExpense){
+      exp += trx->amount;
+    }else{
+      earn += trx->amount;
+    }
+    root = root->nextEra;
+  }
+
+  Trx* trx = root->currentTrx;
+  if(trx->isExpense){
+    exp += trx->amount;
+  }else{
+    earn += trx->amount;
+  }
+
+  cout << "Expenses: " << exp << endl;
+  cout << "Earnings: " << earn << endl;
+  cout << "Balance: " << earn - exp << endl;
+}
+
+void insertEra(TrxEras* eras, Era* newEra){
+  Era* root = eras->rootEra;
+		while(root->nextEra != NULL){
+    root = root->nextEra;
+  }
+  root->nextEra = newEra;
+}
+
+void printEras(TrxEras* eras){
+  Era* root = eras->rootEra;
+  if(root == NULL){
+    cout << "empty list" << endl;
+    return;
+  }
+  cout << "Eras{" << endl;
+  while(root->nextEra != NULL){
+    stampTrx(root->currentTrx);
+    cout << "," << endl;
+    root = root->nextEra;
+  }
+  stampTrx(root->currentTrx);
+  cout << endl << "}" << endl;
+}
+
+TrxEras* insertTrx(TrxEras *eras);
 
 int main(int argc, char *argv[]) {
 
   char *username;
-  char buffer[256];
-  Trx *trx;
+  TrxEras *eras = NULL;
 
   if (argc >= 2) {
     allocString(username, argv[1]);
@@ -50,18 +127,40 @@ int main(int argc, char *argv[]) {
     cout << "Benvenuto come ti chiami?\t";
     username = getInput(username);
   }
-
-  cout << username; 
-
-  insertTrx(trx);
+ 
+  char action;
+  do{
+    cout << "i->newTrx, s->printEra, q->quit: ";
+    cin >> action;
+    switch(action){
+      case 'i':
+        cout << "insert a new transaction" << endl;
+        eras = insertTrx(eras);
+        break;
+      case 's':
+        cout << "printing all the ears until now" << endl;
+        printEras(eras);
+        break;
+      case 'c':
+        cout << "total earning and expense" << endl;
+        totalExp(eras);
+        break;
+      case 'q':
+        cout << "GoodBye " << username << endl;
+        printEras(eras);
+        break;
+      default:
+        cout << "command not codified retry" << endl;
+    }
+  }while(action != 'q');
 
   if(username){
     delete username;
     username = nullptr;
   }
-  if(trx){
-    delete trx;
-    trx = nullptr;
+  if(eras){
+    delete eras;
+    eras = nullptr;
   }
   return 0;
 }
@@ -85,7 +184,7 @@ char* allocString(char *str, char *buffer){
   return str;
 }
 
-void insertTrx(Trx*trx){
+TrxEras* insertTrx(TrxEras *eras){
   double amount;
   cout << "amount:\t";
   cin >> amount;
@@ -98,10 +197,24 @@ void insertTrx(Trx*trx){
   cout << "description:\t";
   char *description;
   description = getInput(description);
+
+  bool isExpense = true;
+  char c;
+  cout << "is it an expense(n: no, otherwise: yes): ";
+  cin >> c;
+  getchar();
+  if(c == 'n'){
+    isExpense = false;
+  }
   
-  trx = newTrx(amount,dateTime,description);
+  Trx *trx = newTrx(amount,dateTime,description,isExpense);
+
+  if(eras == NULL){
+    eras = newTrxEras(newEra(trx));
+  }else{
+    insertEra(eras,newEra(trx));
+  }
  
-  stampTrx(trx);
   if(dateTime){
     delete dateTime;
     dateTime = nullptr;
@@ -110,4 +223,6 @@ void insertTrx(Trx*trx){
     delete description;
     description = nullptr;
   }
+
+  return eras;
 }
